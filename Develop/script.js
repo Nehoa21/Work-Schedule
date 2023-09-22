5bc8af990ee25c73f65cfacacab433c1f042e42d
Nehoa21 <nehoaakina21@yahoo.com>
2023-09-21 18:07:31 -0600

    added current date and some elements

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
+  var textInfo = $('description');
+
+  var saveBtn = $('saveBtn');
+  saveBtn.on('click', function() {
+
+  })
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +29,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd MMMM, D');
+  $('#currentDay').text(currentDate);
 });

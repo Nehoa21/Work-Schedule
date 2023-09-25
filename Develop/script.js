16146365499063a8fd9180d79257d24b38d103a8
Nehoa21 <nehoaakina21@yahoo.com>
2023-09-24 23:30:20 -0600

    finished most of the code, not functioning correctly yet

--- Develop/script.js
+++ Develop/script.js
@@ -8,32 +8,6 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  
-  // TODO: Add code to display the current date in the header of the page.
-  var currentDate = dayjs().format('dddd MMMM D, YYYY h:mm a');
-  $('#currentDay').text(currentDate);
-
-  var textInfo = $('description');
-
-
-  function setHourColor(){
-    var currentHour = getHours();
-    if (currentHour < getHours()) {
-      $('.time-block')
-    }
-  }
-  
-
-  var saveBtn = $('saveBtn');
-  saveBtn.on('click', function() {
-    
-    // var formData = new FormData(textInfo);
-    // var data = Object.fromEntries(formData.entries());
-    // var dataString = JSON.stringify(data);
-    // localStorage.setItem('savedData', dataString);
-  });
-
-
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -45,12 +19,68 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-
-
-
-
-
-
-
   
+  // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd MMMM D, YYYY h:mm a');
+  $('#currentDay').text(currentDate);
+
+  var textInfo = $('description');
+  var timeBlock = $('time-block');
+  var saveBtn = $('saveBtn');
+
+  showTextInfo();
+
+  function setHourColor(){
+    var currentHour = dayjs().hour();
+    console.log (currentHour);
+    timeBlock.each(function() {
+      // timeBlock.addClass('time-block');
+      var time = parseInt($(this).attr("id").split("hour-")[0]);
+      
+      if (time < currentHour) {
+        $(this).addClass("past");
+      } else if (time === currentHour) {
+        $(this).addClass("present");
+      } else if (time > currentHour) {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  function showTextInfo() {
+    var userIn = localStorage.getItem('description');
+
+    if (!userIn) {
+      return;
+    }
+  }
+
+  saveBtn.on('click', function(event) {
+    event.preventDefault();
+    // textInfo = $(this).siblings(textInfo).val();
+    // textTime = $(this).parent().attr("id");
+
+    var infoEl = userIn.value;
+
+    localStorage.setItem('description', infoEl);
+    console.log(textInfo);
+  });
+
+  function init() {
+    // var storedText = JSON.parse(localStorage.getItem('textInfo'));
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+    showTextInfo();
+    setHourColor();
+  }
+  init();
 });
